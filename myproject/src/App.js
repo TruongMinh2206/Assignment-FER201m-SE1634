efae356d5543a959cd4d59d368398e55ae4a1412
Trmingnw <minhnthe161666@fpt.edu.vn>
2023-03-21 11:46:36 +0700

    d
    update

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
+import { createContext, useState } from "react";
+import Register from "./components/Register";
+export const UserContent = createContext();
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [user, setUser] = useState({});
+	return (
+		<UserContent.Provider value={{ setUser, user }}>
+			<Router>
+				<div className="App">
+					<Routes>
+						<Route path="/" element={<Login />} />						
+						<Route path="/albums/:slug" element={<Home />} />
+						<Route path="/detail/:slug/:id" element={<Detail />} />
+						<Route path="/register" element={<Register />} />
+					</Routes>
+				</div>
+			</Router>
+		</UserContent.Provider>
+	);
 }
 
 export default App;
